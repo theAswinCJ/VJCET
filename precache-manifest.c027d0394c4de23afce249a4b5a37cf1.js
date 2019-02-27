3b8945f6d04edeaee2ffee245c41e71ab69d2302
Aswin CJ <aswincj4@gmail.com>
2019-02-27 22:27:58 +0530

    Updates

---
+++ precache-manifest.c027d0394c4de23afce249a4b5a37cf1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff86799f23bfc415805cd0a98c47731f",
+    "url": "/VJCET/static/media/VJCLogo.ff86799f.png"
+  },
+  {
+    "revision": "128405a8683d5cb4e65a",
+    "url": "/VJCET/static/css/main.561f5608.chunk.css"
+  },
+  {
+    "revision": "5328b5ea73fa37c01410",
+    "url": "/VJCET/static/js/runtime~main.5328b5ea.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/VJCET/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "eda530164be45eb5d1239f9ecfea882c",
+    "url": "/VJCET/static/media/CarouselThreeNext.eda53016.svg"
+  },
+  {
+    "revision": "58bad2a8ca95deec7dfd0053e008f0f4",
+    "url": "/VJCET/static/media/CarouselThreePrevious.58bad2a8.svg"
+  },
+  {
+    "revision": "128405a8683d5cb4e65a",
+    "url": "/VJCET/static/js/main.128405a8.chunk.js"
+  },
+  {
+    "revision": "e9ee7c98194a7d23b5219aa103e288d5",
+    "url": "/VJCET/static/media/hamburger.e9ee7c98.svg"
+  },
+  {
+    "revision": "190272891c210bc9e3a42a3ae1fee926",
+    "url": "/VJCET/static/media/nextArrow.19027289.svg"
+  },
+  {
+    "revision": "d20f16fa8f8a339ce1e0088bbbd63806",
+    "url": "/VJCET/static/media/previousArrow.d20f16fa.svg"
+  },
+  {
+    "revision": "230ed48b6b0d5efd84e6cec9ef3870d4",
+    "url": "/VJCET/static/media/stock1.230ed48b.png"
+  },
+  {
+    "revision": "5d1cfa17136984d228811004daebb03b",
+    "url": "/VJCET/static/media/stock2.5d1cfa17.png"
+  },
+  {
+    "revision": "9aea8fe2d8d6aee368850afb021c51cc",
+    "url": "/VJCET/index.html"
+  }
+];
