0cb9cb032e88f7b39db4767d0e10397d61404553
brittosaji <bs1067@gmail.com>
2019-03-02 11:45:26 +0530

    Carousel Four

--- src/HomePageContainers/CarouselFour.js
+++ src/HomePageContainers/CarouselFour.js
@@ -5,11 +5,58 @@ import HeadingTwo from "../Components/Texts/HeadingTwo";
 import HeadingFive from "../Components/Texts/HeadingFive";
 
 class CarouselFour extends Component {
+  state = { currentPage: 0, sliderSize: 3 };
+  componentDidMount() {
+    this.setState({ sliderSize: window.innerWidth < 768 ? 1 : 3 });
+  }
+  rotateLeft = () => {
+    console.log("rotateLeft");
+    console.log(this.state.currentPage);
+    if (this.state.currentPage - 1 < 0) {
+      this.setState({
+        currentPage: Math.floor(this.slides.length / this.state.sliderSize) - 1
+      });
+    } else {
+      this.setState({
+        currentPage: this.state.currentPage - 1
+      });
+    }
+  };
+  rotateRight = () => {
+    console.log("rotateRight");
+    console.log(this.state.currentPage);
+    if (
+      this.state.currentPage + 1 >
+      Math.floor(this.slides.length / this.state.sliderSize) - 1
+    ) {
+      this.setState({
+        currentPage: 0
+      });
+    } else {
+      this.setState({
+        currentPage: this.state.currentPage + 1
+      });
+    }
+  };
   CarouselTitle = {
     instanceID: "CarouselTitle",
     title: "Upcoming Events"
   };
+  Events = {
+    instanceID: "CarouselFourEvents",
+    title: "Upcoming Events"
+  };
+  Previous = {
+    instanceID: "CarouselFourPreviousArrow",
+    image: "CarouselThreePrevious.svg",
+    width: "30px"
+  };
 
+  Next = {
+    instanceID: "CarouselFourNextArrow",
+    image: "CarouselThreeNext.svg",
+    width: "30px"
+  };
   EventOneTitle = {
     instanceID: "EventOneTitle",
     title: "Arts Festival : Drishya 2019"
@@ -42,26 +89,110 @@ class CarouselFour extends Component {
     image: "stock3.png",
     width: "202px"
   };
+  slides = [
+    {
+      Title: {
+        instanceID: "EventOneTitle",
+        title: "Arts Festival : Drishya 2019"
+      },
+
+      Image: {
+        instanceID: "EventOneImage",
+        image: "stock3.png",
+        width: "202px"
+      }
+    },
+    {
+      Title: {
+        instanceID: "EventTwoTitle",
+        title: "Sports 2019"
+      },
+
+      Image: {
+        instanceID: "EventTwoImage",
+        image: "stock3.png",
+        width: "202px"
+      }
+    },
+    {
+      Title: {
+        instanceID: "EventThreeTitle",
+        title: "Tech Fest : Bodhi 2019"
+      },
+
+      Image: {
+        instanceID: "EventThreeImage",
+        image: "stock3.png",
+        width: "202px"
+      }
+    }
+  ];
 
   render() {
     const { instanceID } = this.props.data;
 
     return (
       <div className="CarouselFour" id={instanceID}>
-        <HeadingTwo data={this.CarouselTitle} />
-        <div className="CarouselFourSlider">
-          <div className="CarouselFourSlide" id={instanceID + "SlideOne"}>
-            <ImageOne data={this.EventOneImage} />
-            <HeadingFive data={this.EventOneTitle} />
+        <div className="CarouselFourContentWrapper">
+          <div className="CarouselFourTop">
+            <HeadingTwo data={this.Events} />
+            <div className="CarouselFourArrows">
+              <ImageOne data={this.Previous} onClickAction={this.rotateLeft} />
+              <ImageOne
+                className="CarouselFourArrowRight"
+                data={this.Next}
+                onClickAction={this.rotateRight}
+              />
+            </div>
           </div>
-          <div className="CarouselFourSlide" id={instanceID + "SlideTwo"}>
-            <ImageOne data={this.EventTwoImage} />
-            <HeadingFive data={this.EventTwoTitle} />
+
+          <div className="CarouselFourSlider">
+            {/* <div className="CarouselFourSlide" id={instanceID + "SlideOne"}>
+              <ImageOne data={this.EventOneImage} />
+              <HeadingFive data={this.EventOneTitle} />
+            </div>
+            <div className="CarouselFourSlide" id={instanceID + "SlideTwo"}>
+              <ImageOne data={this.EventTwoImage} />
+              <HeadingFive data={this.EventTwoTitle} />
+            </div>
+            <div className="CarouselFourSlide" id={instanceID + "SlideThree"}>
+              <ImageOne data={this.EventThreeImage} />
+              <HeadingFive data={this.EventThreeTitle} />
+            </div> */}
+            {this.slides.map((item, index, arr) => {
+              if (
+                index < this.state.sliderSize + this.state.currentPage &&
+                index >= this.state.currentPage * this.state.sliderSize
+              ) {
+                return (
+                  <div
+                    className="CarouselFourSlide"
+                    id={instanceID + "SlideThree"}
+                  >
+                    <ImageOne data={item.Image} />
+                    <HeadingFive data={item.Title} />
+                  </div>
+                );
+              }
+            })}
           </div>
-          <div className="CarouselFourSlide" id={instanceID + "SlideThree"}>
-            <ImageOne data={this.EventThreeImage} />
-            <HeadingFive data={this.EventThreeTitle} />
-          </div>
+        </div>
+        <div className="CarouselFourPagination">
+          {this.slides.map((item, index, arr) => {
+            if (index % this.state.sliderSize == 0)
+              return (
+                <div
+                  style={{
+                    height: "6px",
+                    width: "6px",
+                    backgroundColor:
+                      index == this.state.currentPage ? "#109648" : "#aaa",
+                    margin: "5px",
+                    borderRadius: "100%"
+                  }}
+                />
+              );
+          })}
         </div>
       </div>
     );
